adca67805cad581f32529260a5d25326630f3dd4
aspizu <aspizu@protonmail.com>
2025-10-06 03:37:27 +0530

    fix lfs-bootscripts version download

--- lfs-sources.sh
+++ lfs-sources.sh
@@ -7,3 +7,6 @@ tar -xf lfs-packages-12.4.tar -C /mnt/lfs/sources --strip-components=1
 
 # Fix ncurses filename
 mv /mnt/lfs/sources/ncurses-6.5-20250809.tgz /mnt/lfs/sources/ncurses-6.5-20250809.tar.gz
+# Download correct version of lfs-bootscripts
+cd /mnt/lfs/sources
+wget -c https://www.linuxfromscratch.org/lfs/downloads/12.4/lfs-bootscripts-20250827.tar.xz
