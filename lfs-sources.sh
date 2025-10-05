a17b5ab26ad71645e01f1bc79f8eccf6b7d1d5bd
aspizu <aspizu@protonmail.com>
2025-10-06 01:23:03 +0530

    Fix tar extraction command and update ncurses filename extension

--- lfs-sources.sh
+++ lfs-sources.sh
@@ -3,4 +3,7 @@ set -ex
 
 wget -c https://mirror-hk.koddos.net/lfs/lfs-packages/lfs-packages-12.4.tar
 mkdir -p /mnt/lfs/sources
-tar -xf lfs-packages-12.4.tar -C /mnt/lfs/sources
+tar -xf lfs-packages-12.4.tar -C /mnt/lfs/sources --strip-components=1
+
+# Fix ncurses filename
+mv /mnt/lfs/sources/ncurses-6.5-20250809.tgz /mnt/lfs/sources/ncurses-6.5-20250809.tar.gz
