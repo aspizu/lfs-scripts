6d5b9640e80979aab207aa722b4339a7fe4bd1c7
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-07 08:51:29 +0530

    add xorg-protocol-headers

--- aspizu/tinyx.sh
+++ aspizu/tinyx.sh
@@ -3,12 +3,5 @@ pkgopen tinyx
 ./autogen.sh
 ./configure \
     --prefix=/usr \
-    --disable-xres \
-    --disable-screensaver \
-    --disable-xdmcp \
-    --disable-xdm-auth-1 \
-    --disable-dbe \
-    --disable-xf86bigfont \
-    --disable-dpms \
     --disable-static
 pkgclose
