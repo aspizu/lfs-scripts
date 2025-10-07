64e8e9f835fb5c4ce8dbcc37421f105344a4a834
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-07 09:46:54 +0530

    fix libXdmcp

--- blfs/libXdmcp.sh
+++ blfs/libXdmcp.sh
@@ -1,5 +1,5 @@
 pkgfetch https://www.x.org/pub/individual/lib/libXdmcp-1.1.5.tar.xz
-pkgopen
+pkgopen libXdmcp-1.1.5
 ./configure $XORG_CONFIG --docdir=/usr/share/doc/libXdmcp-1.1.5
 make
 make install
